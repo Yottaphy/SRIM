f731fb20c99154b53a9f2c23c076e1124bebfe75
Yottaphy <jorge13romero@gmail.com>
2023-02-10 17:40:52 +0000

    more graphs

--- srimplot.py
+++ srimplot.py
@@ -5,62 +5,68 @@ import math
 import scienceplots
 
 darkgrey = "#000000"
-
 plt.style.use("science")
 
-# filename = "My13um_Ar750mbar_80Zr.txt"
-depth, counts500, _ = np.genfromtxt("My13um_Ar500mbar_80Zr.txt", unpack=True)
-depth, counts750, _ = np.genfromtxt("My13um_Ar750mbar_80Zr.txt", unpack=True)
-depth, counts1000, _ = np.genfromtxt("My13um_Ar1bar_80Zr.txt", unpack=True)
+filename = "213Rn_5umNi/213Rn_5umNi"
+depth, counts500, _ = np.genfromtxt(filename + "_Ar500mbar.txt", unpack=True)
+depth, counts750, _ = np.genfromtxt(filename + "_Ar750mbar.txt", unpack=True)
+depth, counts1000, _ = np.genfromtxt(filename + "_Ar1000mbar.txt", unpack=True)
 depth = depth / 1e7
 
-plt.hist(
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.hist(
     depth,
-    len(counts1000),
+    bins=len(counts1000),
     weights=counts1000,
-    histtype="stepfilled",
+    histtype="step",
     color="black",
     linestyle=":",
     align="left",
     label="1000 mbar",
 )
-plt.hist(
+ax.hist(
     depth,
-    len(counts750),
+    bins=len(counts750),
     weights=counts750,
-    histtype="stepfilled",
+    histtype="step",
     color="dimgrey",
     alpha=0.9,
     linestyle="--",
     align="left",
     label="750 mbar",
 )
-plt.hist(
+ax.hist(
     depth,
-    len(counts500),
+    bins=len(counts500),
     weights=counts500,
-    histtype="stepfilled",
+    histtype="step",
     color="DarkGrey",
     alpha=0.9,
     align="left",
     label="500 mbar",
 )
-# plt.axvline(depth[1], color=darkgrey, linestyle="--")
-# plt.text(
+# ax.axvline(depth[1], color=darkgrey, linestyle="--")
+# ax.text(
 #    depth[1] + 0.25,
 #    0.75 * counts1000.max(),
 #    "Window",
 #    rotation="vertical",
 #    color=darkgrey,
 # )
-plt.xlim(0, 30)
-plt.xlabel("Depth (mm)")
-plt.ylabel("Linear density (cm$^{-1}$)")
-plt.legend()
-# plt.text(4.5, 0.85 * counts.max(), "$^{80}$Zr @ 120$\,$MeV")
-# plt.text(20, 0.75 * counts.max(), "13$\,\mu$m Mylar window")
-# plt.text(5, 0.65 * counts.max(), "Ar @ 750$\,$mbar")
+maximum = max(counts500.max(), counts750.max(), counts1000.max())
 
-# plt.savefig(filename.strip(".txt") + ".pdf", bbox_inches="tight", transparent="True")
+ax.set_xlim(0, 30)
+ax.set_ylim(0, 1.35 * maximum)
+ax.set_xlabel("Depth (mm)")
+ax.set_ylabel("Linear density (cm$^{-1}$)")
 
-plt.savefig("80Zr_13umMylar.pdf", bbox_inches="tight", transparent="True")
+handles, labels = ax.get_legend_handles_labels()
+ax.legend(reversed(handles), reversed(labels), fontsize="small")
+ax.text(2, 1.2 * maximum, "$^{213}$Rn @ 131$\,$MeV", size="smaller")
+ax.text(2, 1.1 * maximum, "5$\,\mu$m Ni window", size="smaller")
+# ax.text(5, 0.65 * counts.max(), "Ar @ 750$\,$mbar")
+
+# ax.savefig(filename.strip(".txt") + ".pdf", bbox_inches="tight", transparent="True")
+
+plt.savefig(filename + ".pdf", bbox_inches="tight", transparent="True")
